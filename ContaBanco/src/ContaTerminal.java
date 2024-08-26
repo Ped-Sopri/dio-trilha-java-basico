dbd021051a5562dfed72e25e396a21f208fab19c
Ped-Sopri <psoprijo@gmail.com>
2024-08-25 23:41:02 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+       Scanner scan = new Scanner(System.in);
+
+        //exibir mensagem para o usuario digitar e obtenção de dados
+        System.out.println("Bem Vindo a sua nova criação de conta bancaria!!!");
+        System.out.println("Por favor digite seu primeiro nome e sobrenome: ");
+        String nome = scan.nextLine();
+
+        System.out.println("Digite sua agencia bancaria: ");
+        String agencia = scan.nextLine();
+        
+        System.out.println("Digite numero da conta: ");
+        int numero = scan.nextInt();
+       
+        System.out.println("Digite seu saldo bancario: ");
+        Double saldo = scan.nextDouble();
+        //mensagem final da conta criada
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+        scan.close();
     }
 }
